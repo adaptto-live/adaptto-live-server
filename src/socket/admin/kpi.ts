7b175838ff7d3a6363759871d5813f1d77e44e74
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-10-21 20:08:02 +0200

    Allow access to Statistics and KPIs for Q&A Admin

--- src/socket/admin/kpi.ts
+++ src/socket/admin/kpi.ts
@@ -5,11 +5,11 @@ import log from '../../util/log'
 import { MessageModel, QAEntryModel, TalkRatingModel, UserModel } from '../../repository/mongodb.schema'
 
 export async function handleAdminKPI(socket : Socket<ClientToServerEvents,ServerToClientEvents,InterServerEvents,SocketData>) {
-  const { admin } = socket.data
+  const { admin, qaadmin } = socket.data
   const minuteSlots = [0, 30]
 
   // admin-only operations
-  if (!admin) {
+  if (!(admin || qaadmin)) {
     return
   }
 
